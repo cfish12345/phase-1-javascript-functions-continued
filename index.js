88254df446742a61948b5639dec16a1dae91ade4
christian fish <cfishmusic@hotmail.com>
2022-04-07 04:54:29 -0500

    commit tree 75674829a45898ad0a475ea3c35375a712c25e00

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function saturdayFun(activity = "roller-skate") {
+    if (typeof activity === undefined) {
+        return "roller-skate";
+    }
+    console.log(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity2 = "go to the office") {
+    console.log(`This Monday, I will ${activity2}.`);
+}
+
+function wrapAdjective() {
+    return function () {
+        const param = "special";
+        return `You are ${param}`
+    }
+}
 // code your solution here
